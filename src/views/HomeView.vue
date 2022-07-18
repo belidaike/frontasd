db4f773b3195331cb87c35d877321ae2ae918ced
unknown <ikebelida156@gmail.com>
2022-07-18 11:47:36 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,56 @@
+<template>
+ 
+    <h1 class="text-center">Home</h1>
+  
+    
+  <div class="row d-flex justify-content-center ">
+    <div class="col-md-2">
+      <div class="card" >
+    <div class="card-body bg-secondary">
+      <h5 class="card-title text-center">User Information</h5>
+      <p class="card-text text-center">
+        Lastname: {{ user.lname }}
+        <br />
+        Firstname: {{ user.fname }}
+        <br />
+        Mobile: {{ user.mobile }}
+        <br />
+
+        Course: {{ user.course }}
+        <br />
+        Year: {{ user.year }}
+        <br />
+      </p>
+      <div class="col-md=12 text-center">
+        <a href="#" class="btn btn-danger text-center">
+        Go Somewhere
+      </a>
+      </div>
+    </div>
+  </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useAuthStore } from "../stores/auth";
+import { useRouter } from "vue-router";
+
+export default {
+  setup() {
+    const { user, token } = useAuthStore();
+    const router = useRouter();
+
+    if (token == "") {
+      router.replace("/login");
+    }
+
+    return {
+      user,
+      token,
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ src/views/Shouts.vue
@@ -0,0 +1,78 @@
+<template>
+  <h1 class="text-center">Shouts Page</h1>
+
+
+  <div class="row">
+    <div class="col-md-2">
+    </div>
+    <div class="col-md-9">
+      <table class="table table-striped bg-secondary">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>User ID</th>
+            <th>Shoutout</th>
+            <th>Tags</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="list in shouts" :key="list.id">
+            <td>{{ list.id }}</td>
+            <td>{{ list.user_id }}</td>
+            <td>{{ list.shoutout }}</td>
+            <td>
+              {{ list.tags }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useAuthStore } from "../stores/auth";
+import { useRouter } from "vue-router";
+import { storeToRefs } from "pinia";
+import { ref } from "vue";
+export default {
+  data() {
+    return {
+      shouts: [],
+      token: "",
+    };
+  },
+  methods: {
+    getData() {
+      const authStore = useAuthStore();
+      const router = useRouter();
+
+      const { token } = storeToRefs(useAuthStore());
+      this.token = token;
+
+      fetch("http://localhost:8000/api/shouts/", {
+        method: "get",
+        headers: {
+          Accept: "application/json",
+          Authorization: "Bearer " + authStore.token,
+        },
+      })
+        .then((data) => data.json())
+        .then((data) => (this.shouts = data))
+        .catch((err) => console.log(err));
+    },
+  },
+
+  mounted() {
+    this.getData();
+    const router = useRouter();
+
+    console.log("mounted");
+    if (this.token == "") {
+      router.replace("/login");
+    }
+  },
+};
+</script>
+
+<style></style>
